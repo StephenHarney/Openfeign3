984c34a9ad041248ddf243408a87e4bcb6a40717
Stephen Harney <g00367573@atu.ie>
2023-10-16 22:03:42 +0100

    ('"' (code 34)): was expecting comma to separate Object entries] dont know how to solve this.

--- src/main/java/ie/atu/hello/UserController.java
+++ src/main/java/ie/atu/hello/UserController.java
@@ -1,10 +1,10 @@
 package ie.atu.hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,22 +15,24 @@ public class UserController {
 
 
     private RegistrationServiceClient registrationServiceClient;
-    private final AcknowledgeService acknowledgeService;
+    //private final AcknowledgeService acknowledgeService;
 
 
     @Autowired
-    public UserController(AcknowledgeService acknowledgeService, RegistrationServiceClient registrationServiceClient){
-        this.acknowledgeService = acknowledgeService;
-        this.registrationServiceClient =registrationServiceClient;
+    public UserController(RegistrationServiceClient registrationServiceClient){
+        //this.acknowledgeService = acknowledgeService;
+        this.registrationServiceClient = registrationServiceClient;
 
     }
 
     @PostMapping("/confirm-and-register")
-    public Map<String, String> confirmRegister(@RequestBody UserDetails userDetails){
+    public Map<String, String> confirmAndController(@RequestBody UserDetails userDetails){
+        //return registrationServiceClient.someDetails(userDetails);
         String confirm = registrationServiceClient.someDetails(userDetails);
-        Map<String,String> responseMessage = new HashMap<>();
-        responseMessage.put("Message",confirm);
-        return responseMessage;
+         //String confirm = "Test;
+         Map<String, String> responseMessage = new HashMap<>();
+         responseMessage.put("Registration", confirm);
+         return responseMessage;
     }
 
 
